cf99892aaf36fe620b8bdce8c0ae259f045ebac5
Rahmat Subandi <rhmtin12@gmail.com>
2021-04-11 18:02:14 +0700

    create database and migrate

---
+++ database/migrations/2021_04_11_101916_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            // membuat table bertype varchar
+            $table->string('name');
+            $table->string('photo');
+            $table->string('slug');
+            // untuk menghapus data namun bisa dikembalikan
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
